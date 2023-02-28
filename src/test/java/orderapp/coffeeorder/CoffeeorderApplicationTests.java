8aa06793a31dd9930c48c5f92dee2fbaddac3a72
kim-ksp7331 <ksp7331@gmail.com>
2023-02-28 11:02:35 +0900

    📰 : create project

---
+++ src/test/java/orderapp/coffeeorder/CoffeeorderApplicationTests.java
@@ -0,0 +1,13 @@
+package orderapp.coffeeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
